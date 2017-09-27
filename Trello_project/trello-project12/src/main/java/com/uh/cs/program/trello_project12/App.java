53ae0e6abd7448cdd4907bd5c2b5ef3bd72426e9
Naresh kumar <nareshhauntin@gmail.com>
2017-09-27 12:40:51 -0500

    Registration page

--- Trello_project/trello-project12/src/main/java/com/uh/cs/program/trello_project12/App.java
+++ Trello_project/trello-project12/src/main/java/com/uh/cs/program/trello_project12/App.java
@@ -1,5 +1,7 @@
 package com.uh.cs.program.trello_project12;
 
+import java.util.Scanner;
+
 import login.Login;
 import login.Register;
 
@@ -11,11 +13,19 @@ public class App
 {
     public static void main( String[] args )
     {
+    	System.out.println("Welcome to Trello \n Enter 1 to Login \n Enter 2 to register");
+    	Scanner inputReader = new Scanner(System.in);
+    	int value = inputReader.nextInt();
+    	if (value == 1) {
     Login user = new Login();
      user.login();
+    	}
+    	else if (value == 2) {
       Register newuser = new Register();
       newuser.register();
     }
+    	else 
+    		System.out.println("Please enter a valid option");
 }
-
+}
 
--- Trello_project/trello-project12/src/main/java/login/Register.java
+++ Trello_project/trello-project12/src/main/java/login/Register.java
@@ -1,21 +1,37 @@
 package login;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Register {
 	
 	public void register()
 	{
-		Scanner inputReader = new Scanner (System.in);
-		System.out.println("Please enter your name:");
-		String username  = inputReader.nextLine();
-		System.out.println("Please enter your emailID:");
-		String email  = inputReader.nextLine();
-		System.out.println("Please enter your desired password");
-		String password = inputReader.nextLine();
-		inputReader.close();
-		System.out.println("Name:"+ username + "Email ID:"+email);
-		inputReader.close();
+		try{  
+			Class.forName("com.mysql.jdbc.Driver");  
+			Connection con=DriverManager.getConnection(  
+			"jdbc:mysql://50.62.176.51/Trello","trello","Team12");
+			Statement s = null;
+			
+			Scanner inputReader = new Scanner (System.in);
+			System.out.println("Please enter your name:");
+			String username  = inputReader.nextLine();
+			System.out.println("Please enter your emailID:");
+			String email  = inputReader.nextLine();
+			System.out.println("Please enter your desired password");
+			String password = inputReader.nextLine();
+			inputReader.close();
+			System.out.println("Name:"+ username + "Email ID:"+email);
+			
+			String values = "INSERT INTO login (user_name,password) " + "VALUES ('" +username+ "', '" +password+"')";
+			s = con.createStatement();
+			s.executeUpdate(values);  
+			System.out.println("Updated Successfully");
 	}
+		catch(Exception e){ System.out.println(e);}  
+}
 
 }
