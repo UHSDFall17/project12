41cf423f751d91a5b0331a17be3d97197019a980
Naresh kumar <nareshhauntin@gmail.com>
2017-09-27 21:03:51 -0500

    Changed login page

--- Trello_project/trello-project12/src/main/java/login/Login.java
+++ Trello_project/trello-project12/src/main/java/login/Login.java
@@ -13,25 +13,25 @@ public class Login {
 			Class.forName("com.mysql.jdbc.Driver");  
 			Connection con=DriverManager.getConnection(  
 			"jdbc:mysql://50.62.176.51/Trello","trello","Team12");   
-			Statement s=con.createStatement();  
-			ResultSet rs=s.executeQuery("select * from login");  
+			
 			
 			Scanner inputReader = new Scanner (System.in);
 			System.out.println("Please enter your username:");
 			String username  = inputReader.nextLine();
 			System.out.println("Please enter your password");
 			String password = inputReader.nextLine();
-		// comments
+			
+			String values = "Select user_name,password from login Where user_name ='"+username+"' and password = '" +password+"';";
+			Statement s=con.createStatement();  
+			ResultSet rs=s.executeQuery(values); 
 			
 			if(rs.next()) {
-				
-			if (username.equals(rs.getString(1)) && password.equals(rs.getString(2))) {
 				System.out.println("Successful Login");	
 			}
 			else {
 			System.out.println("Invalid Login");
 			}    
-			}
+			
 			con.close();
 		}
 		catch(Exception e){ System.out.println(e);}  
