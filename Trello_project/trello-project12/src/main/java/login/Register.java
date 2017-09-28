fa21f12a273b73975709203c4f4d5db83b755f86
DESKTOP-3FUI3FP\Rajeshwari <raji92.banu@gmail.com>
2017-09-27 21:08:46 -0500

    Required Filed Validation for Register Page

--- Trello_project/trello-project12/src/main/java/login/Register.java
+++ Trello_project/trello-project12/src/main/java/login/Register.java
@@ -8,6 +8,23 @@ import java.util.Scanner;
 
 public class Register {
 	
+	public void RequiredValidation(int a)
+	{
+	
+		switch(a)
+		{
+		case 1: System.out.println ("Name is Required!. Try Again");
+		register();
+		break;
+		case 2: System.out.println ("Email is Required!. Try Again");
+		register();
+		break;
+		case 3: System.out.println ("Password is Required!. Try Again");
+		register();
+		break;
+		}
+	}
+	
 	public void register()
 	{
 		try{  
@@ -17,19 +34,35 @@ public class Register {
 			Statement s = null;
 			
 			Scanner inputReader = new Scanner (System.in);
+			System.out.println("---Registration Form---");
 			System.out.println("Please enter your name:");
 			String username  = inputReader.nextLine();
-			System.out.println("Please enter your emailID:");
-			String email  = inputReader.nextLine();
-			System.out.println("Please enter your desired password");
-			String password = inputReader.nextLine();
-			inputReader.close();
-			System.out.println("Name:"+ username + "Email ID:"+email);
+			 if (username.length()==0)
+		    {
+				 RequiredValidation(1);
+		    }
+				 
+				System.out.println("Please enter your emailID:");
+				String email  = inputReader.nextLine();
+				 if (email.length()==0)
+				    {
+						 RequiredValidation(2);
+				    }
+				System.out.println("Please enter your desired password");
+				String password = inputReader.nextLine();
+				 if (password.length()==0)
+				    {
+						 RequiredValidation(3);
+				    }
+				inputReader.close();
+				System.out.println("Name:"+ username + "Email ID:"+email);
+				
+				String values = "INSERT INTO login (user_name,password) " + "VALUES ('" +username+ "', '" +password+"')";
+				s = con.createStatement();
+				s.executeUpdate(values);  
+				System.out.println("Updated Successfully");
+			 
 			
-			String values = "INSERT INTO login (user_name,password) " + "VALUES ('" +username+ "', '" +password+"')";
-			s = con.createStatement();
-			s.executeUpdate(values);  
-			System.out.println("Updated Successfully");
 	}
 		catch(Exception e){ System.out.println(e);}  
 }
