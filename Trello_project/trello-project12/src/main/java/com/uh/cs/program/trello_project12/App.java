fd314597e94961e764b57ffc512e3fca35aee028
DESKTOP-3FUI3FP\Rajeshwari <raji92.banu@gmail.com>
2017-09-29 19:20:15 -0500

    Revert "Added forgotpassword() function and modifyied the main function."
    This reverts commit bb1d0f61c62a9d74a4d742c4b92a9d3f4af661f1.

--- Trello_project/trello-project12/src/main/java/com/uh/cs/program/trello_project12/App.java
+++ Trello_project/trello-project12/src/main/java/com/uh/cs/program/trello_project12/App.java
@@ -13,7 +13,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	System.out.println("Welcome to Trello \n Enter 1 to Login \n Enter 2 to register \n Enter 3 for New Password\n");
+    	System.out.println("Welcome to Trello \n Enter 1 to Login \n Enter 2 to register");
     	Scanner inputReader = new Scanner(System.in);
     	int value = inputReader.nextInt();
     	if (value == 1) {
@@ -24,10 +24,6 @@ public class App
       Register newuser = new Register();
       newuser.register();
     }
-    	else if(value == 3) {
-    		Login user = new Login();
-    		user.forgotPassword();
-    	}
     	else 
     		System.out.println("Please enter a valid option");
 }
