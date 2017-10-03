bec87393243ec09077b1237e314cdf7aba817b3b
DESKTOP-3FUI3FP\Rajeshwari <raji92.banu@gmail.com>
2017-10-02 20:31:31 -0500

    Team creation functionality implementation

--- Trello_project/trello-project12/src/main/java/board/Team.java
+++ Trello_project/trello-project12/src/main/java/board/Team.java
@@ -1,9 +1,33 @@
 package board;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class Team {
 
 	public void team()
 	{
-		System.out.println("Team Page");
+		
+		try {
+		Class.forName("com.mysql.jdbc.Driver");  
+		Connection con=DriverManager.getConnection(  
+		"jdbc:mysql://50.62.176.51/Trello","trello","Team12"); 
+		Statement s = null;
+		
+		Scanner inputReader = new Scanner (System.in);
+		System.out.println("---Create Team---");
+		System.out.println("Please enter Team Name:");
+		String teamname  = inputReader.nextLine();
+		System.out.println("Please enter Team Description(optional):");
+		String teamdesc  = inputReader.nextLine();
+		String values = "INSERT INTO team (team_name,team_desc) " + "VALUES ('" +teamname+ "', '" +teamdesc+"')";
+		s = con.createStatement();
+		s.executeUpdate(values);  
+		System.out.println(teamname + "Team created Successfully");
+		}
+		catch(Exception e){ System.out.println(e);} 
 	}
 }
