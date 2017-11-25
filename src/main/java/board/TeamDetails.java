cdf32c9ce532503f921c2bac9afcb2282905c791
DESKTOP-3FUI3FP\Rajeshwari <raji92.banu@gmail.com>
2017-11-25 12:11:08 -0600

    Adding all properties for Team Module

--- src/main/java/board/TeamDetails.java
+++ src/main/java/board/TeamDetails.java
@@ -1,11 +1,19 @@
 package board;
 
+import java.util.HashMap;
 import java.util.List;
+import sqlStatements.TeamSqlQueries;
 
 public class TeamDetails {
 	private String teamname;
     private String teamdesc;
-    private List<String> members;;
+    private List<String> members;
+    private HashMap<String, String> teamDetails;
+    private List<String> teamList;
+    private String memberName;
+    private String allMemberNames;
+    private int option;
+    TeamSqlQueries sqlObj=new TeamSqlQueries();
     
     public TeamDetails()
     {
@@ -19,7 +27,8 @@ public class TeamDetails {
 
      public String getTeamName() {
         return teamname;
-     }     
+     }
+     
       public void setTeamDesc(String teamdesc) {
           this.teamdesc = teamdesc;
        }
@@ -32,5 +41,39 @@ public class TeamDetails {
         }
        public List<String> getMembers() {
            return members;
-        }        
+        }
+       public void setTeamDetails(HashMap<String, String> teamDetails) {
+           this.teamDetails = teamDetails;
+        }
+       public HashMap<String, String> getTeamDetails() {
+           return teamDetails;
+        }
+       public void setTeamList(List<String> teamList) {
+           this.teamList = teamList;
+        }
+       public List<String> getTeamList() {
+           return teamList;
+        }
+       public void setMemberName(String memberName) {
+           this.memberName = memberName;
+        }
+
+        public String getMemberName() {
+           return memberName;
+        }
+        public void setOption(int option) {
+            this.option = option;
+         }
+
+         public int getOption() {
+            return option;
+         }
+         public void setAllMemberNames(String allMemberNames) {
+             this.allMemberNames = allMemberNames;
+          }
+         
+          public String getAllMemberNames() {
+             return allMemberNames;
+          }
+         
 }
