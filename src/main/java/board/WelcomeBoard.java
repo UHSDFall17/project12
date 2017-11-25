1544a8bbc5f6d71e385e82a241a8325518469b79
DESKTOP-3FUI3FP\Rajeshwari <raji92.banu@gmail.com>
2017-11-25 14:38:53 -0600

    line alignment change for welcome baord file

--- src/main/java/board/WelcomeBoard.java
+++ src/main/java/board/WelcomeBoard.java
@@ -7,36 +7,40 @@ import trello.App;
 public class WelcomeBoard {
 	int value;
 	Scanner inputReader = new Scanner(System.in);
-	public void welcome()
-	{
-		
-    	do {
-    		System.out.println("Select the options below \n 1.Create Board \n 2.Team \n 3.Create Business Board \n 4. Display Boards \n 5. Logout");
-    		
-        	value = inputReader.nextInt();
-        	inputReader.nextLine();
-    	switch(value)
-    	{
-    	case 1: Board b = new Board();
-		b.board();
-		break;
-    	case 2: Team t = new Team();
-		t.team();
-		break;
-    	case 3:Board c = new Board();
-		c.board();
-		break;
-    	case 4: ViewBoard vb = new ViewBoard();
-    	vb.displayBoards();
-    	case 5: 
-    		App a = new App();
-    		a.options();
-		break;
-    	default: System.out.println("invalid option");
-    	break;
-    	}
-    	}	while(value != 5);
-    
+
+	public void welcome() {
+
+		do {
+			System.out
+					.println("Select the options below \n 1.Create Board \n 2.Team \n 3.Create Business Board \n 4. Display Boards \n 5. Logout");
+
+			value = inputReader.nextInt();
+			inputReader.nextLine();
+			switch (value) {
+			case 1:
+				Board b = new Board();
+				b.board();
+				break;
+			case 2:
+				Team t = new Team();
+				t.team();
+				break;
+			case 3:
+				Board c = new Board();
+				c.board();
+				break;
+			case 4:
+				ViewBoard vb = new ViewBoard();
+				vb.displayBoards();
+			case 5:
+				App a = new App();
+				a.options();
+				break;
+			default:
+				System.out.println("invalid option");
+				break;
+			}
+		} while (value != 5);
+
 	}
-	}
-	
+}
