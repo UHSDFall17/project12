e7802a0b405ba61447795294f886329840d01a81
Naresh kumar <nareshkumar7593@gmail.com>
2017-11-26 17:52:25 -0600

    Create Board functionality

--- src/main/java/board/Board.java
+++ src/main/java/board/Board.java
@@ -13,99 +13,79 @@ import trello.ConnectionManager;
 
 public class Board {
 	private Connection con = null;
-	Scanner inputReader = new Scanner (System.in);
+	Scanner inputReader = new Scanner(System.in);
 	BoardSqlQueries sqlObj = new BoardSqlQueries();
-	String boardname,boardname1;int value ;
+	String boardname, boardname1;
+	int value;
 	WelcomeBoard wb = new WelcomeBoard();
-	
-public void option() {
-	do {
-	System.out.println("Select the options below \n 1.Create Board \n 2. Display Boards \n 3. Star Board \n 4. Go Back");
-	value = inputReader.nextInt();
-	inputReader.nextLine();
-	switch (value) {
-	case 1:
-		board();
-		break;
-	case 2:
-		displayBoards();
-		break;
-	case 3:
+
+	public void option() {
+		do {
+			System.out.println(
+					"Select the options below \n 1.Create Board \n 2. Display Boards \n 3. Star Board \n 4. Go Back");
+			value = inputReader.nextInt();
+			inputReader.nextLine();
+			switch (value) {
+			case 1:
+				createBoard();
+				break;
+			case 2:
+				displayBoards();
+				break;
+			case 3:
+				starBoard();
+			case 4:
+				wb.welcome();
+				break;
+			default:
+				System.out.println("invalid option");
+				break;
+			}
+		} while (value != 4);
+	}
+
+	public void createBoard() {
+
+		System.out.println("Creeate Board \n Enter the title of the board");
+		String title = inputReader.nextLine();
+		System.out.println("Enter the team name to assign");
+		String team_name = inputReader.nextLine();
+		sqlObj.createBoard(title,team_name);
+	}
+
+	public void displayBoards() {
+		sqlObj.viewboards();
 		starBoard();
-	case 4:
-		wb.welcome();
-		break;
-	default:
-		System.out.println("invalid option");
-		break;
+
+		System.out.println("Do you wish to open board");
+
+		String open = inputReader.nextLine();
+		if (open.equals("yes")) {
+			System.out.println("Please enter the name of the board which you wish to open");
+			boardname = inputReader.nextLine();
+			openBoard(boardname);
+		} else
+			System.out.print("Back functionality");
+
+		Cards card = new Cards();
+		card.cardDetails();
 	}
-	}while (value != 4);
-}
-	
-public void board() {
 
-	System.out.println("Creeate Board \n Enter the title of the board");
-	String title = inputReader.nextLine();
-	try {
-		con = ConnectionManager.getConnection();
-		Statement s=con.createStatement();  
-		ResultSet rs=s.executeQuery("Select team_name from team");
-			if(rs.next() != true) {
-				System.out.println("There are no teams,Do you wish to create one");
-			}
-			else {
-				System.out.println("Would you like to select the team for the Board \n Type 'yes' to select team \n Type 'no' to proceed further ");
-			}
-			String input = inputReader.nextLine();
-			if(input.equals("yes")) {
-				while(rs.next()) {
-					System.out.println(rs.getString(1));
-				}
-				Team t = new Team();
-				t.team();	
-			}
-	else {
-		System.out.println("Please enter a valid option");
+	private void openBoard(String boardname2) {
+		this.boardname1 = boardname2;
+		sqlObj.listCards(boardname1);
+
 	}
+
+	private void starBoard() {
+		System.out.println("Do you wish to Star any of your boards");
+		String option = inputReader.nextLine();
+		if (option.equals("yes")) {
+			System.out.println("Please enter the name of the board which you wish to star");
+			boardname = inputReader.nextLine();
+			sqlObj.starIt(boardname);
+		} else
+			System.out.println("No star operation");
 	}
-	catch(Exception e){ System.out.println(e);} 
-}
-
-public void displayBoards() {
-	sqlObj.viewboards();
-	starBoard();
-
-	System.out.println("Do you wish to open board");
-
-	String open = inputReader.nextLine();
-	if (open.equals("yes")) {
-		System.out
-				.println("Please enter the name of the board which you wish to open");
-		boardname = inputReader.nextLine();
-		openBoard(boardname);
-	} else
-		System.out.print("Back functionality");
-	
-	Cards card = new Cards();
-	card.cardDetails();
-}
-
-private void openBoard(String boardname2) {
-	this.boardname1 = boardname2;
-	sqlObj.listCards(boardname1);
-	
-}
-
-private void starBoard() {
-	System.out.println("Do you wish to Star any of your boards");
-	String option = inputReader.nextLine();
-	if (option.equals("yes")) {
-		System.out
-				.println("Please enter the name of the board which you wish to star");
-		boardname = inputReader.nextLine();
-		sqlObj.starIt(boardname);
-	} else
-		System.out.println("No star operation");
-}
 
 }
--- src/main/java/sqlStatements/BoardSqlQueries.java
+++ src/main/java/sqlStatements/BoardSqlQueries.java
@@ -8,71 +8,80 @@ import trello.ConnectionManager;
 
 public class BoardSqlQueries {
 	private Connection con;
-	Statement s = null;String name,starred;String lists;
+	Statement s = null;
+	String name, starred;
+	String lists;
 	private String boardnames1;
 	private String starboard;
-	public void viewboards()
-	{
-		try{
-		con = ConnectionManager.getConnection();	
-		Statement s=con.createStatement(); 
-		
-		   System.out.println("Starred Board");
-		      System.out.println("--------------");
-		      ResultSet rs1=s.executeQuery("SELECT * FROM board WHERE starred = '1'");
-		      while(rs1.next()){
-		    	  starred = rs1.getString("boardname");
-		    	  System.out.println("Boardname: " + starred);
-		      }
-		      
-		System.out.println("Personal Board");
-		System.out.println("--------------");
-		ResultSet rs=s.executeQuery("SELECT * FROM board");
-	      
-			while(rs.next()){
-	         name = rs.getString("boardname");
-	         System.out.println("Boardname: " + name);
-	      }
-	    
-	      }
 
-		
-		catch(Exception e){ System.out.println(e);}
+	public void viewboards() {
+		try {
+			con = ConnectionManager.getConnection();
+			Statement s = con.createStatement();
+
+			System.out.println("Starred Board");
+			System.out.println("--------------");
+			ResultSet rs1 = s.executeQuery("SELECT * FROM board WHERE starred = '1'");
+			while (rs1.next()) {
+				starred = rs1.getString("boardname");
+				System.out.println("Boardname: " + starred);
+			}
+
+			System.out.println("Personal Board");
+			System.out.println("--------------");
+			ResultSet rs = s.executeQuery("SELECT * FROM board");
+
+			while (rs.next()) {
+				name = rs.getString("boardname");
+				System.out.println("Boardname: " + name);
+			}
+
+		}
+
+		catch (Exception e) {
+			System.out.println(e);
+		}
 	}
-	
-	
-	
-	
-	
+
 	public void listCards(String boardnames) {
 		this.boardnames1 = boardnames;
-		try{
-			con = ConnectionManager.getConnection();	
-			Statement s=con.createStatement(); 
-			ResultSet rs=s.executeQuery("SELECT * FROM board WHERE boardname ='" +boardnames1+ "'");
-			while(rs.next()){
-		         lists = rs.getString("list");
-		         System.out.println("List: " + lists);
-		    	  	}	
-		      }
-		      
-		catch(Exception e){ System.out.println(e);}
+		try {
+			con = ConnectionManager.getConnection();
+			Statement s = con.createStatement();
+			ResultSet rs = s.executeQuery("SELECT * FROM board WHERE boardname ='" + boardnames1 + "'");
+			while (rs.next()) {
+				lists = rs.getString("list");
+				System.out.println("List: " + lists);
+			}
+		}
+
+		catch (Exception e) {
+			System.out.println(e);
+		}
 	}
-
-
-
-
 
 	public void starIt(String boardname) {
 		this.starboard = boardname;
-		try{
-			con = ConnectionManager.getConnection();	
-			Statement s=con.createStatement(); 
-			s.executeUpdate("UPDATE board SET starred = '1' WHERE boardname ='" +starboard+ "'");
-				System.out.println("Starred Successfully");
-		      }
-		catch(Exception e){ System.out.println(e);}
-		
+		try {
+			con = ConnectionManager.getConnection();
+			Statement s = con.createStatement();
+			s.executeUpdate("UPDATE board SET starred = '1' WHERE boardname ='" + starboard + "'");
+			System.out.println("Starred Successfully");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
 	}
 	
+	public void createBoard(String title,String team_name) {
+		try {
+			con = ConnectionManager.getConnection();
+			String values = "INSERT INTO baord (boardname,team_name) " + "VALUES ('" +title+ "', '" +team_name+"')";
+			s = con.createStatement();
+			s.executeUpdate(values); 
+			System.out.println("Created Board Succesfully");
+		}catch (Exception e) { System.out.println(e);}
+		}
+
+
 }
